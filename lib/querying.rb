be7638e41f99aecb4acef73d7d34cb935a527866
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-30 01:02:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_series_title_with_most_human_characters
     ON character_books.book_id = books.id
     JOIN characters
     ON characters.id = character_books.character_id
-    WHERE characters.species = "human" 
+    WHERE characters.species = 'human' 
     GROUP BY series.title 
     ORDER BY COUNT(characters.id) DESC 
     LIMIT 1;"
